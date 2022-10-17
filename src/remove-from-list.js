0bb095119abd0973585095f2d2e58f623aa68ef0
vrNKg <veronika.gamalia@mail.ru>
2022-10-17 23:38:31 +0400

    ready task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -21,10 +21,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  *     this.next = null;
  *   }
  * }
+ * 
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function removeKFromList(l, k) {
+//   if (l == null) return null;
+//   let root = l;
+//   while(root != null) {
+//     if(root.value === k){
+//       if(root.next != null) {
+//         root.value = root.next.value;
+//         root.next = root.next.next
+//       } else {
+//         root = null;
+//       }
+//     } else{
+//       root = root.next;
+//     };
+//   }
+
+//   return l;
+// }
+
+// function removeKFromList(l, k) {
+    
+//   let root = l;
+
+//   while(root){
+//     if(root.value === k){            
+//       if(null===null){
+//           root = root.next
+//           continue
+//       } else {
+//           null.next = root.next
+//           root = rootnext
+//           continue
+//       }
+//     }
+//     root = root.next
+//   }
+//   return l
+//   }
+function removeKFromList(l, k) {
+    
+  let root = l;
+  let previous = null;
+
+  while(root){
+    if(root.value === k){            
+      if(previous===null){
+          root = l = root.next;
+          continue
+      } else {
+          previous.next = root.next;
+          root = root.next;
+          continue
+      }
+    }
+    previous = root;
+    root = root.next;
+  }
+  return l;
 }
 
 module.exports = {
